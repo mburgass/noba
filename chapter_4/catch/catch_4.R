967f6bb89bb986d2b0b978718ea5c029511abc95
mburgass <m.burgass14@imperial.ac.uk>
2019-01-29 11:26:15 +0000

    update catch chapter 4

--- chapter_4/catch/catch_4.R
+++ chapter_4/catch/catch_4.R
@@ -13,8 +13,42 @@ read.csv("chapter_4/catch/fmsy_1.1_catch.csv")%>% select(Year, REO, GRH, MAC, HA
 
 fmsy_catch<- rbind(catch06, catch08, catch1, catch1.1)
 
+write.csv(fmsy_catch, "chapter_4/catch/catch_all_years.csv", row.names = F)
+
 fmsy_catch %>% filter(Year> 2014) %>% ggplot(aes(Year, catch)) +geom_line(aes(colour=species))+
   facet_wrap(~scenario)
 
 fmsy_catch %>% filter(Year> 2014) %>% ggplot(aes(Year, catch)) + geom_line(aes(colour=scenario)) +
   facet_wrap(~species, scales="free")
+
+total_catch<- fmsy_catch %>% group_by(scenario, Year) %>% summarise(total_catch=sum(catch)) %>% ungroup()%>%
+  group_by(scenario) %>% summarise(total_catch=sum(total_catch))
+
+write.csv(total_catch, "chapter_4/catch/total_catch2.csv")
+
+####ADD IN PRICE/TON#####
+
+usd_ton<- read.csv("usd_ton.csv")
+fmsy_catch %>% left_join(usd_ton, by="species") %>% mutate(total_usd=catch*USD) %>% group_by(scenario, Year) %>%
+  summarise(total_usd=sum(total_usd))->catch_price
+
+catch_price_total<- catch_price%>% group_by(scenario) %>% summarise(total_usd=sum(total_usd2))
+
+catch_price %>% filter(Year>2014) %>% ggplot(aes(Year, total_usd))+geom_line(aes(colour=scenario), lwd=1.5)+
+  ylab("Catch Price (USD)")
+
+write.csv(catch_price_total, "chapter_4/catch/total_price.csv", row.names = F)
+
+write.csv(catch_price, "chapter_4/catch/price_year.csv", row.names=F)
+
+
+
+#######Before/After######
+
+read.csv("chapter_4/catch/before_after.csv")-> before_after
+
+ggplot(before_after,aes(x=species,y=catch,fill=scenario))+
+  geom_bar(stat="identity",position="dodge")+
+  scale_fill_brewer(palette="Dark2")+
+  xlab("Species")+ylab("Catch")+
+  ggtitle("Average annual catch of fished species under different scenarios compared to historical catch")
